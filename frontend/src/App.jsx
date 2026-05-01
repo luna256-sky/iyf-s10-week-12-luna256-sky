7d0cb88f65bdd19f4ce4c979780eaf9fc60fb870
Dolla Grace <dollagraceambwaya@gmail.com>
2026-05-02 02:38:50 +0300

    feat: income module - IncomeForm, IncomeList, API integration and README update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,26 @@
+import IncomeForm from "./components/IncomeForm";
+import IncomeList from "./components/IncomeList";
+import { useState, useEffect } from "react";
+
 function App() {
+  const [transactions, setTransactions] = useState([]);
+
+  const addTransaction = (transaction) => {
+    setTransactions((prev) => [...prev, transaction]);
+  };
+
+  useEffect(() => {
+    fetch("http://localhost:3000/api/transactions")
+      .then((response) => response.json())
+      .then((data) => setTransactions(data));
+  }, []);
+
   return (
     <div>
       <h1>Finance Tracker 💰</h1>
+      <IncomeForm onAddIncome={addTransaction} />
+
+      <IncomeList transactions={transactions} />
     </div>
   );
 }
